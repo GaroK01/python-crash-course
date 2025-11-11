288fc0be83092aec31d96bb425f8ffdf04cb6a51
Garo Kalfayan <64748704+GaroK01@users.noreply.github.com>
2025-11-11 21:22:05 +0200

    Create user.py

---
+++ Book Exercises/Chapter 9/user.py
@@ -0,0 +1,40 @@
+
+class User:
+
+  def __init__(self, first_name, last_name, address, mobile_number):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.address = address
+    self.mobile_number = mobile_number
+    self.login_attempts = 0
+
+  def describe_user(self):
+    print(f"I will provide some info about {self.first_name} {self.last_name}")
+    print(f"Address: {self.address} \nMobile number: {self.mobile_number}")
+
+  def greet_user(self):
+    print(f"Hello dear, {self.first_name}!")
+
+  def increment_login_attempts(self):
+    self.login_attempts += 1
+
+  def reset_login_attempts(self):
+    self.login_attempts = 0
+
+class Admin(User):
+  def __init__(self, first_name, last_name, username, email, location):
+    super().__init__(first_name, last_name, username, email, location)
+    self.privileges = Privileges()
+
+
+class Privileges:
+  def __init__(self, privileges=[]):
+    self.privileges = privileges
+
+  def show_privileges(self):
+    print("\nPrivileges:")
+    if self.privileges:
+      for privilege in self.privileges:
+        print(f"-{privilege}")
+    else:
+      print("-This user has no privileges.")
